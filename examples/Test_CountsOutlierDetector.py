d19e9a7781db146496a960a37f19c49a17fcdbfa
Brett-Kennedy <84815610+Brett-Kennedy@users.noreply.github.com>
2023-08-13 21:23:56 -0400

    Add files via upload

---
+++ examples/Test_CountsOutlierDetector.py
@@ -0,0 +1,396 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import cProfile
+from sklearn.datasets import make_classification
+from sklearn.datasets import load_iris, load_breast_cancer, load_wine, load_diabetes
+from sklearn.datasets import fetch_openml
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import sys
+sys.path.insert(0, 'C:\\python_projects\\DatasetsEvaluator_project\\DatasetsEvaluator')
+import DatasetsEvaluator as de
+
+sys.path.insert(0, 'C:\\python_projects\\CountsOutlierDetection_project\\CountsOutlierDetector')
+from counts_outlier_detector import CountsOutlierDetector
+
+'''
+This file evaluates the presence of outliers in 3+ dimensions in the openml.org dataset collection
+'''
+
+np.random.seed(0)
+
+pd.options.display.max_columns = 1000
+pd.options.display.max_colwidth = 1000
+pd.options.display.max_rows = 1000
+pd.options.display.width = 10000
+
+PROFILE_RUN = False
+RUN_PARALLEL = True
+MAX_NUM_COMBINATIONS = 100_000_000
+NUM_DATASETS_EVALUATED = 100  #todo: seems to stop at 124 when set to 200
+TEST_SINGLE_DATASET = ""
+#TEST_SINGLE_DATASET = "mushroom"
+
+results_folder = "c:\\outlier_results"
+
+
+def print_header(dataset_index, dataset_name):
+    stars = "********************************************"
+    msg = f"\n\n{stars}\n{dataset_index}: {dataset_name}\n{stars}"
+    return msg
+
+
+# todo: add a 2nd method to get the data for numeric columns
+def load_categorical_datasets():
+    cache_folder = "c:\\dataset_cache"
+
+    if TEST_SINGLE_DATASET == "synth_1":
+        return generate_synth_1()
+
+    datasets_tester = de.DatasetsTester(problem_type="both", path_local_cache=cache_folder)
+    if TEST_SINGLE_DATASET:
+        matching_datasets = datasets_tester.find_by_name([TEST_SINGLE_DATASET])
+    else:
+        matching_datasets = datasets_tester.find_datasets(
+            use_cache=True,
+            min_num_features=0,
+            max_num_features=np.inf,
+            min_num_instances=500,
+            max_num_instances=50_000,
+            min_num_numeric_features=0,
+            max_num_numeric_features=np.inf,  
+            min_num_categorical_features=3,
+            max_num_categorical_features=np.inf)
+
+    print("Number matching datasets found: ", len(matching_datasets))
+
+    # Note: some datasets may have errors loading or testing.
+    datasets_tester.collect_data(max_num_datasets_used=NUM_DATASETS_EVALUATED,
+                                 # Files known to not load properly, excluding to reduce time
+                                 # todo: I'm using the automatic exclude list feature instead, should be able to remove this
+                                 exclude_list=['PCam16k', 'realAdExchange2', 'FOREX_cadchf-day-High', 'lotto',
+                                               'titanic_1', 'dataset_time_3', 'FOREX_eurgbp-day-Close',
+                                               'FOREX_eurdkk-day-Close'],
+                                 use_automatic_exclude_list=True,
+                                 max_cat_unique_vals=np.inf,
+                                 check_local_cache=True,
+                                 check_online=True,   # Don't generally set to False, but sometimes to be faster
+                                 save_local_cache=True,
+                                 one_hot_encode=False)
+    return datasets_tester.get_dataset_collection()
+
+
+def run_tests(datasets):
+    # todo: call print_header in here, not in the detector. i've moved the func to this file.
+
+    run_summary_df = None
+    for dataset_dict in datasets:
+        dataset_index = dataset_dict['Index']
+        dataset_name = dataset_dict['Dataset_name']
+        x = dataset_dict['X']
+        det = CountsOutlierDetector(max_num_combinations=MAX_NUM_COMBINATIONS, results_folder=results_folder, results_name=dataset_name, run_parallel=RUN_PARALLEL)
+        flagged_rows_df, row_explanations, output_msg, dataset_run_summary_df = det.predict(x)
+        if run_summary_df is None:
+            run_summary_df = dataset_run_summary_df
+            run_summary_df.insert(0, 'Dataset Index', dataset_index)
+            run_summary_df.insert(1, 'Dataset Name', dataset_name)
+        else:
+            run_summary_df = run_summary_df.append(dataset_run_summary_df, ignore_index=True)
+            run_summary_df.loc[run_summary_df.index[-1], 'Dataset Index'] = dataset_index
+            run_summary_df.loc[run_summary_df.index[-1], 'Dataset Name'] = dataset_name
+        print(output_msg)
+    return run_summary_df
+
+
+def test_data(datasets):
+    global RUN_PARALLEL
+
+    # Profiling is not possible with multiprocessing, as the code executed by processes other than the main
+    # are not included.
+    if PROFILE_RUN:
+        RUN_PARALLEL = False
+        with cProfile.Profile() as pr:
+            run_summary_df = run_tests(datasets)
+        pr.print_stats()
+    else:
+        t1 = datetime.now()
+        run_summary_df = run_tests(datasets)
+        t2 = datetime.now()
+        print("\n\n\nTime for total run: ", t2-t1)
+
+    # Save run_summary_df
+    n = datetime.now()
+    dt_string = n.strftime("%d_%m_%Y_%H_%M_%S")
+    file_name = results_folder + "\\run_summary_" + dt_string + ".csv"
+    run_summary_df.to_csv(file_name)
+
+    return run_summary_df
+
+
+# todo: set max_dimensions as a parameter
+
+def output_run_summary(run_summary_df):
+    sns.set()
+
+    print(run_summary_df)
+    run_summary_df = run_summary_df.dropna()
+    if len(run_summary_df) == 0:
+        return
+
+    print("avg Percent Flagged as 1d: ", run_summary_df['Percent Flagged as 1d'].astype(float).mean())
+    print("avg Percent Flagged as 2d: ", run_summary_df['Percent Flagged as 2d'].astype(float).mean())
+    print("avg Percent Flagged as 3d: ", run_summary_df['Percent Flagged as 3d'].astype(float).mean())
+    print("avg Percent Flagged as 4d: ", run_summary_df['Percent Flagged as 4d'].astype(float).mean())
+    print("avg Percent Flagged as 5d: ", run_summary_df['Percent Flagged as 5d'].astype(float).mean())
+    print("avg Percent Flagged as 6d: ", run_summary_df['Percent Flagged as 6d'].astype(float).mean())
+
+    ############################################################################
+    # Plot the average number of rows flagged at each dimension as a bar graph
+    heights_arr = [run_summary_df['Percent Flagged as 1d'].astype(float).mean(),
+                   run_summary_df['Percent Flagged as 2d'].astype(float).mean(),
+                   run_summary_df['Percent Flagged as 3d'].astype(float).mean(),
+                   run_summary_df['Percent Flagged as 4d'].astype(float).mean(),
+                   run_summary_df['Percent Flagged as 5d'].astype(float).mean(),
+                   run_summary_df['Percent Flagged as 6d'].astype(float).mean()]
+    fig, ax = plt.subplots()
+    ax.bar(
+        x=range(1, 7), # todo: in this and other plots, have a variable for the max dimensions examined.
+        height=heights_arr,
+        tick_label=[f"avg % Flagged examining {x}d: " for x in range(1, 7)]
+    )
+    plt.xticks(rotation=45, ha='right')
+    ax.set_yticks(range(0, 110, 10))
+    ax.set_title("Average percent of rows flagged at each dimension")
+    results_plot_filename = results_folder + "\\counts_by_dim_bar.png"
+    fig.savefig(results_plot_filename, bbox_inches='tight', dpi=150)
+
+    ############################################################################
+    # Plot the average number of rows flagged examining up to each dimension as a bar graph
+    heights_arr = [run_summary_df['Percent Flagged up to 1d'].astype(float).mean(),
+                   run_summary_df['Percent Flagged up to 2d'].astype(float).mean(),
+                   run_summary_df['Percent Flagged up to 3d'].astype(float).mean(),
+                   run_summary_df['Percent Flagged up to 4d'].astype(float).mean(),
+                   run_summary_df['Percent Flagged up to 5d'].astype(float).mean(),
+                   run_summary_df['Percent Flagged up to 6d'].astype(float).mean()]
+    fig, ax = plt.subplots()
+    ax.bar(
+        x=range(1, 7),
+        height=heights_arr,
+        tick_label=[f"avg % Flagged examining up to {x}d: " for x in range(1, 7)]
+    )
+    plt.xticks(rotation=45, ha='right')
+    ax.set_yticks(range(0, 110, 10))
+    ax.set_title("Average percent of rows flagged up to and including each dimension")
+    results_plot_filename = results_folder + "\\counts_up_to_dim_bar.png"
+    fig.savefig(results_plot_filename, bbox_inches='tight', dpi=150)
+
+    ############################################################################
+    # Plot the percent of rows flagged in some way as the # of dimensions examined is increased
+    # for each dataset, drawing one line per dataset.
+    fig, ax = plt.subplots()
+    for r in range(len(run_summary_df)):
+        row = run_summary_df.iloc[r]
+        x_coords = list(range(1, 7))
+        y_coords = [row['Percent Flagged up to 1d'],
+                    row['Percent Flagged up to 2d'],
+                    row['Percent Flagged up to 3d'],
+                    row['Percent Flagged up to 4d'],
+                    row['Percent Flagged up to 5d'],
+                    row['Percent Flagged up to 6d']]
+        y_coords = [float(x) for x in y_coords]
+        ax.plot(x_coords, y_coords, label=row['Dataset Name'])
+    fig.tight_layout()
+    ax.set_xticks(range(1, 7))
+    ax.set_xticklabels([str(x) for x in range(1, 7)])
+    ax.legend(bbox_to_anchor=(1.05, 1))
+    ax.set_xlabel("Number dimensions")
+    ax.set_ylabel("% of Rows Flagged")
+    ax.set_title("Percent of Rows Flagged Given # Dimensions Examined")
+    results_plot_filename = results_folder + "\\count_up_to_dim_line.png"
+    fig.savefig(results_plot_filename, bbox_inches='tight', dpi=150)
+
+    ############################################################################
+    # Plot the number of cases where don't check because too many combinations
+    heights_arr = [100.0,
+                   100.0,
+                   run_summary_df['Checked_3d'].sum() * 100.0 / len(run_summary_df),
+                   run_summary_df['Checked_4d'].sum() * 100.0 / len(run_summary_df),
+                   run_summary_df['Checked_5d'].sum() * 100.0 / len(run_summary_df),
+                   run_summary_df['Checked_6d'].sum() * 100.0 / len(run_summary_df)
+                   ]
+    fig, ax = plt.subplots()
+    ax.bar(
+        x=range(1, 7),
+        height=heights_arr,
+        tick_label=[f"% Datasets examining up to {x}d: " for x in range(1, 7)]
+    )
+    plt.xticks(rotation=45, ha='right')
+    ax.set_yticks(range(0, 110, 10))
+    ax.set_title("Percent of Datasets Examining Each Dimension (not skipped due to size limits)")
+    results_plot_filename = results_folder + "\\percent_datasets_checked_by_dim_bar.png"
+    fig.savefig(results_plot_filename, bbox_inches='tight', dpi=150)
+
+    # todo: plot the number of cases where don't check because the expected under uniform is too low
+
+    # todo: plot the number of cases where don't check because the expected given marginal distribs is too low
+
+
+def sythetic_test_1():
+    """
+    This is a simple demonstration of the CountsOutlierDetector API. It creates a dataset with 5000 rows, 10 numeric
+    columns with uniform distributions. It explicitly sets one outlier in Row 0 and 2 (unrelated) outliers in Row 1.
+    It then calls CountsOutlierDetector's predict() API then other APIs to explain the results.
+    """
+
+    det = CountsOutlierDetector(results_folder='Results', results_name='toy', verbose=True)
+
+    a = np.random.uniform(low=0.0, high=100.0, size=(5_000, 10))
+
+    # Create a row with one unusual value
+    a[0, 0] = 1000.0
+
+    # Create a row with two unusual values
+    a[1, 0] = 1100.0
+    a[1, 1] = 1200.0
+
+    # Create a row with one unusual pair of values. We first set column 4 to be the same as 3 in most cases but one,
+    # Row 2, where they have different values.
+    a[:, 4] = a[:, 3]
+    a[2, 3] = 90.0
+    a[2, 4] = 5.0
+
+    # It is not necessary to pass a pandas dataframe, but this lets us specify column names
+    a = pd.DataFrame(a, columns=['Column A', 'Column B', 'Column C', 'Column D', 'Column E', 'Column F', 'Column G',
+                                 'Column H', 'Column I', 'Column J'])
+
+    results = det.predict(a)
+
+    print()
+    print("Breakdown All Rows:")
+    print(results['Breakdown All Rows'])
+
+    print()
+    print("Breakdown Flagged Rows:")
+    print(results['Breakdown Flagged Rows'])
+
+    print()
+    print("Flagged Summary:")
+    print(results['Flagged Summary'])
+
+    print()
+    print("Most Flagged Rows:")
+    most_flagged_df = det.get_most_flagged_rows()
+    print(most_flagged_df)
+
+    det.print_run_summary()
+
+    print()
+    print("Scores Distribution:")
+    det.plot_scores_distribution()
+
+    print()
+    det.explain_row(1)
+
+    print()
+    det.explain_row(2)
+
+
+def sythetic_test_2():
+    np.random.seed(0)
+    x, y = make_classification(
+        n_samples=500,
+        n_features=6,
+        n_informative=1,
+        n_redundant=3,
+        n_repeated=1,
+        n_clusters_per_class=1)
+    x = pd.DataFrame(x)
+
+    # Set 3 random rows to random values, setting 1 to 3 columns to realistic, but different values
+    x.at[0, 0] = x.iloc[1, 0]
+
+    x.at[1, 0] = x.iloc[10, 0]
+    x.at[1, 1] = x.iloc[11, 1]
+
+    x.at[2, 0] = x.iloc[30, 0]
+    x.at[2, 1] = x.iloc[31, 1]
+    x.at[2, 2] = x.iloc[32, 1]
+
+    det = CountsOutlierDetector()
+    results = det.predict(x)
+
+    print()
+    print("Most Flagged Rows:")
+    most_flagged_df = det.get_most_flagged_rows()
+    print(most_flagged_df)
+
+    det.explain_row(1)
+
+
+def synthetic_test_categorical():
+    a_vals = ['a1', 'a2']
+    b_vals = ['b1', 'b2', 'b3']
+    c_vals = ['c1', 'c2', 'c3', 'c4']
+    d_vals = ['d1', 'd2', 'd3', 'd4', 'd5']
+    nrows = 10_000
+    df = pd.DataFrame({
+        'a': np.random.choice(a_vals, nrows, p=[0.9, 0.1]),
+        'b': np.random.choice(b_vals, nrows, p=[0.9, 0.09, 0.01]),
+        'c': np.random.choice(c_vals, nrows, p=[0.1, 0.2, 0.3, 0.4]),
+        'd': np.random.choice(d_vals, nrows, p=[0.1, 0.1, 0.1, 0.2, 0.5]),
+    })
+
+    det = CountsOutlierDetector()
+    results = det.predict(df)
+    print("Flagged Summary:")
+    print(results['Flagged Summary'])
+
+    print()
+    print("Most Flagged Rows:")
+    most_flagged_df = det.get_most_flagged_rows()
+    print(most_flagged_df)
+
+    det.explain_row(5123)
+
+
+def real_test_1():
+    x, y = load_breast_cancer(return_X_y=True, as_frame=True)
+    det = CountsOutlierDetector()
+    results = det.predict(x)
+
+    #det.get_most_flagged_rows().head()
+    det.explain_row(422)
+    det.plot_scores_distribution()
+
+
+def real_test_2():
+    datasets = load_categorical_datasets()
+    run_summary_df = test_data(datasets)
+    output_run_summary(run_summary_df)
+
+
+def real_test_openml():
+    data = fetch_openml('abalone', version=1)
+    df = pd.DataFrame(data.data, columns=data.feature_names)
+
+    det = CountsOutlierDetector(n_bins=3, max_num_combinations=100_000, verbose=True)
+    results = det.predict(df)
+    print(results['Flagged Summary'])
+
+    most_flagged_df = det.get_most_flagged_rows()
+    if most_flagged_df is not None:
+        print(most_flagged_df.head())
+
+    det.explain_features(['Diameter', 'Shucked_weight', 'Shell_weight'])
+    # det.explain_row(3409)
+
+
+if __name__ == "__main__":
+    # sythetic_test_1()
+    # sythetic_test_2()
+    # synthetic_test_categorical()
+    # real_test_1()
+    real_test_openml()
+
